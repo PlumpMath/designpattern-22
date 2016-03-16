52ffc215cd44c387679e4b967ea4b1b46546fd11
raylee2007 <lilinghui-123@163.com>
2016-03-16 08:28:12 +0800

    从头认识设计模式-策略模式-01-原始应用场景

---
+++ src/main/java/com/raylee/designpattern/strategy/ch01/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.raylee.designpattern.strategy.ch01;
+
+/**
+ * @TODO 用于用户的增删改查
+ * @author raylee
+ * @date 2016年3月16日
+ * @version 1.0
+ */
+public class UserServiceImpl extends BaseServiceImpl {
+
+}
