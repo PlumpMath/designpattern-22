00c4b12657541bf7fdfdca7b3ad2d3213c85bc93
raylee2015 <lilinghui-123@163.com>
2016-03-17 11:11:04 +0800

    从头认识设计模式-策略模式-04-引入设计原则：分离变化与不变的部分 补充Client的测试部分

--- src/main/java/com/raylee/designpattern/strategy/ch04/Client.java
+++ src/main/java/com/raylee/designpattern/strategy/ch04/Client.java
@@ -2,4 +2,15 @@ package com.raylee.designpattern.strategy.ch04;
 
 public class Client {
 	// 对用户、部门、课程进行增删改查
+		public static void main(String[] args) {
+			CourseServiceImpl courseServiceImpl = new CourseServiceImpl();
+			courseServiceImpl.importCourse();
+			courseServiceImpl.exportCourse();
+			UserServiceImpl userServiceImpl = new UserServiceImpl();
+			userServiceImpl.importUser();
+			userServiceImpl.exportUser();
+			DeptServiceImpl deptServiceImpl = new DeptServiceImpl();
+			deptServiceImpl.importDept();
+			deptServiceImpl.exportDept();
+		}
 }
